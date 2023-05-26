c8ae7f741cb9a3279ad55f92e76b80f057102def
morantaDev <morantaDev@gmail.com>
2023-05-26 13:37:42 +0000

    new commit

--- management/commands/execute_tables.py
+++ management/commands/execute_tables.py
@@ -1,8 +1,11 @@
 from django.core.management.base import BaseCommand
 from reservations.views import CREATE_volTable
+from reservations.views import CREATE_carTable
 
 class Command(BaseCommand):
     help = 'Create the Vol table in the database.'
 
     def handle(self, *args, **options):
         CREATE_volTable()
+        CREATE_carTable()
+        
--- reservations/views.py
+++ reservations/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from . import forms
+import json
+import requests
 
 def HomePage(request):
     return render(request, 'index.html',{'title': "PAGE D'ACCUEIL"})
@@ -30,7 +32,7 @@ from django.db import connection
 def CREATE_volTable():
     with connection.cursor() as cursor:
         cursor.execute("""
-            CREATE TABLE reservation_Vol (
+            CREATE TABLE reservations_Vol (
                 id INT auto_increment NOT NULL,
                 type VARCHAR(255) NOT NULL,
                 source VARCHAR(100) NOT NULL,
@@ -58,10 +60,70 @@ def CREATE_volTable():
               values=(item['type'], item['source'],item['itineraries'][0]['segments'][0]['departure']['at'],item['itineraries'][0]['segments'][0]['arrival']['at'], item['price']['total'], item['itineraries'][0]['segments'][0]['departure']['iataCode'],item['itineraries'][0]['segments'][0]['arrival']['iataCode'])
               cursor.execute(
                 '''
-                INSERT INTO Vol(type, source, heure_depart, heure_arrivee, prix, aeroport_depart, aeroport_arrivee)
+                INSERT INTO reservations_Vol(type, source, heure_depart, heure_arrivee, prix, aeroport_depart, aeroport_arrivee)
                 VALUES (%s, %s, %s, %s, %s, %s, %s)
                 ''', values
               )
         except ResponseError as error:
             raise error
+    
+
+
+
+def random_car(request):
+    data = []
+    image_url = ""
+    for _ in range(1000):
+        marque = random.choice(CAR_BRANDS)
+        modele = fake.word()
+        for image in image_corres:
+            if marque == image['name']:
+                image_url = image['url']
+
+        voiture = {
+            'marque': marque,
+            'modele': modele,
+            'annee': random.randint(2000, 2023),
+            'prix_journee': random.uniform(50, 300),
+            'image_url': image_url
+        }
+        data.append(voiture)
+    response = JsonResponse(data, safe=False)
+
+    return response
+
+response = random_car(requests)
+content = response.content.decode('utf-8')
+print(len(content))
+data = json.loads(content)
+print(len(data))
+def CREATE_carTable():
+  with connection.cursor() as cursor:
+    try:
+      cursor.execute('''
+          CREATE TABLE reservations_Car(
+            id INT auto_increment NOT NULL,
+            marque VARCHAR(50) NOT NULL,
+            modele VARCHAR(25) NOT NULL,
+            annee INT(4) NOT NULL,
+            prix_journee DECIMAL(10, 4) NOT NULL,
+            image_url VARCHAR(255) NOT NULL,
+            CONSTRAINT pk_car PRIMARY KEY (id)
+            );
+            ''')
+      for car in data:
+          marque = car['marque']
+          modele = car['modele']
+          annee = car['annee']
+          prix_journee = car['prix_journee']
+          image_url = car['image_url']
+          values=(marque, modele, annee, prix_journee, image_url)
+          
+          cursor.execute('''
+            INSERT INTO reservations_Car(marque, modele, annee, prix_journee, image_url)
+            VALUES(%s, %s, %s, %s, %s)
+            
+                        ''',values)
+    except ResponseError as error:
+      raise error
         
